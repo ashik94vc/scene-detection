683a56f8b2d755839e6291313f47783f9ddd8bf0
Ashik Vetrivelu <vcashik@gmail.com>
2018-03-07 18:40:35 -0500

    Added few lines to classifier

--- main/classifier.py
+++ main/classifier.py
@@ -3,11 +3,24 @@ import theano
 from theano import tensor as T
 from model import model
 from scipy.misc import toimage
+from scipy.optimize import fmin_cg as conjugate_gradient
 
-def error(Y_actual, Y_output):
-    Y = Y_actual - Y_output
-    error = (1/2)*(T.transpose(Y)*Y)
-    return error
+class Classifier(object):
 
-def negative_log_likelihood(Y):
-    return -T.mean(T.log(Y)[T.arange(Y.shape[0]), Y])
+    def __init__(self, dataset):
+        self.dataset = dataset
+        self.train_x,self.train_y = dataset[0]
+        self.test_x,self.test_y = dataset[1]
+        self.batch_size = 1
+
+    def error(self, Y_actual, Y_output):
+        Y = Y_actual - Y_output
+        error = (1/2)*(T.transpose(Y)*Y)
+        return error
+
+    def train():
+        for x in train_x:
+            
+
+    def negative_log_likelihood(Y):
+        return -T.mean(T.log(Y)[T.arange(Y.shape[0]), Y])
