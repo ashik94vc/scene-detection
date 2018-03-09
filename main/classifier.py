Added Predict Function    - Added Predict Function    - Fixed the testing process. Fixed Error computation to fix the dimensions of error

main/classifier.py
@@ -1,10 +1,10 @@
-from model import train as conv_nn_train
-from model import predict as conv_nn_predict
+from model import Model
 import theano
 from theano import tensor as T
 from scipy.misc import toimage
 from scipy.optimize import fmin_cg as conjugate_gradient
 import numpy as np
+import pickle
 
 class Classifier(object):
 
@@ -15,11 +15,12 @@ class Classifier(object):
         self.learning_rate = 0.16
         self.eps = 2e-9
         self.params = {}
+        self.model = Model()
 
     def error(self, Y_actual, Y_output):
         Y = Y_actual - Y_output
-        print(Y.shape)
-        error = (1/2)*(T.transpose(Y)*Y)
+        error = (1/Y.shape[0])*np.sum(Y*Y.T)
+        print(error.shape)
         return error
 
     def compute_gradients(self, cost, parameters):
@@ -38,7 +39,7 @@ class Classifier(object):
     def train(self):
         parameters = None
         for i in range(len(self.train_x)):
-            cost = conv_nn_train([self.train_x[i]],[self.train_y[i]])
+            cost = self.model.train([self.train_x[i]],[self.train_y[i]])
             if i%100 == 0:
                 print(str(i)+" datas trained")
         self.params = parameters
@@ -48,13 +49,11 @@ class Classifier(object):
         Y_predict = list()
         if parameters != None:
             self.params = parameters
-        for x in self.test_x:
-            y_predict = conv_nn_predict([x],self.params)
-            print('hello')
-            print(y_predict)
-            Y_predict.append(y_predict)
+        for x in self.test_x[:100]:
+            y_predict = self.model.predict([x])
+            Y_predict.append(np.asscalar(y_predict))
         Y_predict = np.asarray(Y_predict)
-        error = self.error(self.test_y,Y_predict)
+        error = self.error(self.test_y[:100],Y_predict)
         return error
 
     def cost_function(self, y_predict, y_label):

main/main.py
@@ -6,6 +6,23 @@ import numpy as np
 import re
 from math import floor
 import sys
+from model import Model
+
+def quantizer(value):
+    if value > 0.5:
+        return 1
+    return 0
+
+def confidence(value):
+    if value > 0.5:
+        return value*100
+    return (1.0-value)*100
+
+def num_to_label(input_value):
+    if input_value > 0.5:
+        return "a car!"
+    else:
+        return "not a car!"
 
 #build data for cars
 regex = re.compile("^cars_[0-9]{5}\.jpg$")
@@ -44,5 +61,17 @@ parameters = None
 if len(sys.argv) < 2:
     classifier.train()
 else:
-    parameters = np.load(sys.argv[1])
-error = classifier.test()
+    parameters = np.load(sys.argv[1],allow_pickle=True)
+    print(type(parameters))
+    print(parameters.shape)
+    classifier.model = Model(parameters)
+if sys.argv[2] != None:
+    predict_input = (np.asarray(Image.open(open(sys.argv[2], 'rb')),dtype="float64")/256).transpose(2,0,1)
+    y_output = Model(parameters)
+    y_output_scalar = np.asscalar(y_output)
+    print(y_output_scalar)
+    string_output = "I am {:.3f}% sure that it's a ".format(confidence(y_output_scalar))
+    print(string_output+num_to_label(y_output_scalar))
+else:
+    error = classifier.test()
+    print(error)

main/model.py
@@ -6,90 +6,70 @@ from theano.tensor.signal.pool import pool_2d
 from PIL import Image
 from scipy.misc import toimage
 
-def RMSprop(cost, parameters, learning_rate=0.011, rho=0.8, epsilon=1e-6):
-    gradients = tensor.grad(cost, parameters)
-    updates = []
-    for param, grad in zip(parameters, gradients):
-        acc = theano.shared(param.get_value() * 0.)
-        acc_new = rho * acc + (1 - rho) * grad ** 2
-        gradient_scaling = tensor.sqrt(acc_new + epsilon)
-        grad = grad / gradient_scaling
-        updates.append((acc, acc_new))
-        updates.append((param, param - learning_rate * grad))
-    return updates
+class Model(object):
+    def __init__(self,parameters=None):
+        X = tensor.tensor4()
+        Y = tensor.vector()
+        self.params = parameters
+        if parameters == None:
+            W1 = theano.shared(np.random.randn(6,3,9,9))
+            b1 = theano.shared(np.zeros(6,).astype(theano.config.floatX))
+            W2 = theano.shared(np.random.randn(32,6,9,9))
+            b2 = theano.shared(np.zeros(32,).astype(theano.config.floatX))
+            W3 = theano.shared(np.random.randn(64,32,9,9))
+            b3 = theano.shared(np.zeros(64,).astype(theano.config.floatX))
+            W5 = theano.shared(np.random.randn(7744,84))
+            # b5 = theano.shared(np.zeros(64*9*9,))
+            W6 = theano.shared(np.random.randn(84,1))
+            # b6 = theano.shared(np.zeros(84,))
+        else:
+            print("Hello darkness my old friend")
+            W1 = theano.shared(parameters["W1"])
+            b1 = theano.shared(parameters["b1"])
+            W2 = theano.shared(parameters["W2"])
+            b2 = theano.shared(parameters["b2"])
+            W3 = theano.shared(parameters["W3"])
+            b3 = theano.shared(parameters["b3"])
+            W5 = theano.shared(parameters["W5"])
+            W6 = theano.shared(parameters["W6"])
 
-__X = tensor.tensor4()
-Y = tensor.vector()
-__W1 = theano.shared(np.random.randn(6,3,9,9))
-__b1 = theano.shared(np.zeros(6,).astype(theano.config.floatX))
-__W2 = theano.shared(np.random.randn(32,6,9,9))
-__b2 = theano.shared(np.zeros(32,).astype(theano.config.floatX))
-__W3 = theano.shared(np.random.randn(64,32,9,9))
-__b3 = theano.shared(np.zeros(64,).astype(theano.config.floatX))
-__W5 = theano.shared(np.random.randn(7744,84))
-# __b5 = theano.shared(np.zeros(64*9*9,))
-__W6 = theano.shared(np.random.randn(84,1))
-# __b6 = theano.shared(np.zeros(84,))
-# if parameters == None:
-#     parameters = {}
-#
-#     parameters["W1"] = __W1
-#     parameters["b1"] = __b1
-#     parameters["W2"] = __W2
-#     parameters["b2"] = __b2
-#     parameters["W3"] = __W3
-#     parameters["b3"] = __b3
-#     parameters["W5"] = __W5
-#     parameters["W6"] = __W6
-# else:
-#     __W1 = parameters["W1"]
-#     __b1 = parameters["b1"]
-#     __W2 = parameters["W2"]
-#     __b2 = parameters["b2"]
-#     __W3 = parameters["W3"]
-#     __b3 = parameters["b3"]
-#     __W5 = parameters["W5"]
-#     __W6 = parameters["W6"]
+        layer_1 = conv2d(X,W1)
+        layer_1_pool = pool_2d(layer_1,(2,2),ignore_border=True)
+        layer_1_output = relu(layer_1_pool+b1.dimshuffle('x', 0, 'x', 'x'))
 
+        layer_2 = conv2d(layer_1_output, W2)
+        layer_2_pool = pool_2d(layer_2,(2,2),ignore_border=True)
+        layer_2_output = relu(layer_2_pool+b2.dimshuffle('x', 0, 'x', 'x'))
 
-__layer_1 = conv2d(__X,__W1)
-__layer_1_pool = pool_2d(__layer_1,(2,2),ignore_border=True)
-__layer_1_output = relu(__layer_1_pool+__b1.dimshuffle('x', 0, 'x', 'x'))
+        layer_3 = conv2d(layer_2_output, W3)
+        layer_3_pool = pool_2d(layer_3,(2,2),ignore_border=True)
+        layer_3_output = relu(layer_3_pool+b3.dimshuffle('x', 0, 'x', 'x'))
 
-__layer_2 = conv2d(__layer_1_output, __W2)
-__layer_2_pool = pool_2d(__layer_2,(2,2),ignore_border=True)
-__layer_2_output = relu(__layer_2_pool+__b2.dimshuffle('x', 0, 'x', 'x'))
+        layer_4 = layer_3_output.flatten(2)
 
-__layer_3 = conv2d(__layer_2_output, __W3)
-__layer_3_pool = pool_2d(__layer_3,(2,2),ignore_border=True)
-__layer_3_output = relu(__layer_3_pool+__b3.dimshuffle('x', 0, 'x', 'x'))
+        layer_5 = tensor.dot(layer_4,W5)
+        layer_5_output = layer_5.tanh()
 
-__layer_4 = __layer_3_output.flatten(2)
+        layer_6 = tensor.dot(layer_5_output, W6)
+        layer_6_output = sigmoid(layer_6)
 
-__layer_5 = tensor.dot(__layer_4,__W5)
-__layer_5_output = __layer_5.tanh()
+        cost = -(Y*np.log(layer_6_output) + (1-Y)*np.log(1-layer_6_output)).sum()
 
-__layer_6 = tensor.dot(__layer_5_output, __W6)
-__layer_6_output = sigmoid(__layer_6)
+        parameters = [W1,b1,W2,b2,W3,b3,W5,W6]
 
-    # if y_label != None:
-    #     grads = {}
-    #     cost = -(y_label*np.log(__layer_6_output) + (1-y_label)*np.log(1-__layer_6_output)).sum()
-    #     grads["dW1"] = tensor.grad(cost,__W1).eval()
-    #     grads["db1"] = tensor.grad(cost,__b1).eval()
-    #     grads["dW2"] = tensor.grad(cost,__W2).eval()
-    #     grads["db2"] = tensor.grad(cost,__b2).eval()
-    #     grads["dW3"] = tensor.grad(cost,__W3).eval()
-    #     grads["db3"] = tensor.grad(cost,__b3).eval()
-    #     grads["dW5"] = tensor.grad(cost,__W5).eval()
-    #     grads["dW6"] = tensor.grad(cost,__W6).eval()
-    #     return cost.eval(),parameters,grads
-    # return __layer_6_output.eval(),parameters
-cost = -(Y*np.log(__layer_6_output) + (1-Y)*np.log(1-__layer_6_output)).sum()
+        updates = self.RMSprop(cost,parameters)
 
-parameters = [__W1,__b1,__W2,__b2,__W3,__b3,__W5,__W6]
+        self.train = theano.function([X, Y], cost,updates=updates)
+        self.predict = theano.function([X],layer_6_output)
 
-updates = RMSprop(cost,parameters)
-
-train = theano.function([__X, Y], cost,updates=updates)
-predict = theano.function([__X, params],__layer_6_output)
+    def RMSprop(self, cost, parameters, learning_rate=0.011, rho=0.8, epsilon=1e-6):
+        gradients = tensor.grad(cost, parameters)
+        updates = []
+        for param, grad in zip(parameters, gradients):
+            acc = theano.shared(param.get_value() * 0.)
+            acc_new = rho * acc + (1 - rho) * grad ** 2
+            gradient_scaling = tensor.sqrt(acc_new + epsilon)
+            grad = grad / gradient_scaling
+            updates.append((acc, acc_new))
+            updates.append((param, param - learning_rate * grad))
+        return updates